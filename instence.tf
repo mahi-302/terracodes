ad8a24613bc9d68375d7b83ce7a38f801664c2f8
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-04 00:07:54 +0530

    Update instence.tf

--- instence.tf
+++ instence.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "abcd" {
      ami = "ami-0fb653ca2d3203ac1"
      instance_type = "t2.micro"
-     availability_zone = "us-east-2"
+     availability_zone = "us-east-2c"
      subnet_id = "subnet-02ac0c9e52da309ab"
      tags = {
 	name = "mahe1"
