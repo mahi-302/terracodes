b0d2c9e777abff46c2b6107f98853584b11c0198
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-04 23:04:56 +0530

    Update instence.tf

--- instence.tf
+++ instence.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "abcd" {
      instance_type = "t2.micro"
      availability_zone = "us-east-2c"
      subnet_id = "subnet-02ac0c9e52da309ab"
-	user-data = "${file("userdata.sh")}"
+	user_data = "${file("userdata.sh")}"
      tags = {
 	name = "mahe1"
 	}
